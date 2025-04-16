79e2bfa9343c7693018a663f7c4da9cec8967497
Davi Ferreira de Souza <davi.f.souza@unesp.br>
2025-04-16 14:27:26 -0300

    shunting yard

---
+++ 3o Semestre/Estrutura de Dados 1/Shunting Yard/shuntingyard.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int prioridade(char c) {
+    if (c == '^') return 4;
+    if (c == '*' || c == '/') return 3;
+    if (c == '+' || c == '-') return 2;
+    return 0;
+}
+
+bool e_operando(const string& str) {
+    string operadores = "+-*/^()";
+    return operadores.find(str) == string::npos;
+}
+
+string infixa_pos_fixa(const string& expressao) {
+    string retorno = "";
+    string operadores_string = "^*/+-";
+    stack<string> operadores;
+    queue<string> output;
+
+    stringstream ss(expressao);
+    string token;
+    vector<string> tokens;
+
+    while (ss >> token) 
+    {
+        tokens.push_back(token);
+    }
+
+    for (string str : tokens) 
+    {
+        if (e_operando(str)) {
+            output.push(str);
+        } 
+        else if (operadores_string.find(str) != string::npos) 
+        {
+            while (!operadores.empty() &&
+                   prioridade(operadores.top()[0]) >= prioridade(str[0]) &&
+                   operadores.top() != "(") 
+            {
+                output.push(operadores.top());
+                operadores.pop();
+            }
+            operadores.push(str);
+        } 
+        else if (str == "(") 
+        {
+            operadores.push(str);
+        } 
+        else if (str == ")") 
+        {
+            while (!operadores.empty() && operadores.top() != "(") 
+            {
+                output.push(operadores.top());
+                operadores.pop();
+            }
+            if (!operadores.empty()) operadores.pop();
+        }
+    }
+
+    while (!operadores.empty()) 
+    {
+        output.push(operadores.top());
+        operadores.pop();
+    }
+
+    while (!output.empty()) {
+        retorno += output.front() + " ";
+        output.pop();
+    }
+
+    return retorno;
+}
+
+int main() {
+    string expressao;
+    getline(cin, expressao);
+
+    cout << infixa_pos_fixa(expressao) << endl;
+    return 0;
+}
